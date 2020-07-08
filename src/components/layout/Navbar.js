d28df6d2bb7d127d3c7fb241078aaf50afd409e2
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-07-08 23:51:42 +0530

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+
+import SignedOutLinks from "./SignedOutLinks";
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="nav-wrapper grey darken-3">
+        <div className="container">
+          <Link to="/" className="brand-logo">
+            Mario Plan
+          </Link>
+          <SignedInLinks />
+          <SignedOutLinks />
+        </div>
+      </nav>
+    </div>
+  );
+}
